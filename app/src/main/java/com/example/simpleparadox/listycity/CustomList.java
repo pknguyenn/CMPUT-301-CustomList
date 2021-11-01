cd7f1106665f4d436f03d87c2df27a2aebc10104
pknguyenn <78003230+pknguyenn@users.noreply.github.com>
2021-11-01 02:29:59 -0600

    RED: Added add city test (should fail)

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -47,4 +47,13 @@ public class CustomList extends ArrayAdapter<City> {
         return view;
 
     }
+
+    @Override
+    public int getCount() {
+        return cities.size();
+    }
+
+    public void addCity(City city){
+
+    }
 }
---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,24 @@
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+
+    private CustomList list;
+
+    @BeforeEach
+    public void createList() {
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest() {
+
+    }
+
+
+}
